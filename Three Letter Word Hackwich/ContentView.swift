d69137fba5396621151ba011969cb10bdf61dc41
Amelia <atan@ucls.uchicago.edu>
2021-07-27 10:47:07 -0500

    Created tap gesture recognizer

--- Three Letter Word Hackwich/ContentView.swift
+++ Three Letter Word Hackwich/ContentView.swift
@@ -18,6 +18,15 @@ struct ContentView: View {
                 .bold()
                 .padding()
             Text("Tap the gray box to change the letter")
+            CustomLetterBox(color: Color.gray, text: letter)
+                .onTapGesture {
+                    let position = alphabet.index(alphabet.startIndex, offsetBy: counter)
+                    letter = String(alphabet[position])
+                    counter += 1
+                    if counter == alphabet.count {
+                        counter = 0
+                    }
+                }
         }
     }
 }
